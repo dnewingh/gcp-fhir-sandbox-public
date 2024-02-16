925ee8d01ff469b24cf71895fd7257ce5c842206
Dan Newingham <dannewingham@gmail.com>
2024-02-16 20:19:34 +0000

    WIP: capability statement route

--- cloud-function-fhir-server-proxy/routes/fhirstore-routes.js
+++ cloud-function-fhir-server-proxy/routes/fhirstore-routes.js
@@ -4,10 +4,31 @@ const google = require('@googleapis/healthcare');
 //initialize Router object from Express
 const router = express.Router();
 
+//initialize constants
+fhirStorePath = 'https://healthcare.googleapis.com/v1/projects/gcp-fhir-sandbox-lab-001/locations/us-central1/datasets/GCP-FHIR-Sandbox-Lab-001-Healthcare-Dataset-001/fhirStores/FHIRSTORE-001'
+
+//initialize gcloud authorization client
+const oauthOptions = {
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+  };
+const authClient = new google.auth.GoogleAuth(oauthOptions);
+
 //define handlers
-function getMetadata(req, res) {
+async function getMetadata(req, res) {
     // TODO: Integrate with FHIR store
-    res.send(['capability', 'statement']);
+    //res.send(['capability', 'statement']);
+
+    try {
+        const resourceType = 'metadata';
+        const url = fhirStoreUrl + '/fhir/' + resourceType;
+        const params = req.query;
+        const client = await authClient.getClient();
+        const response = await client.request({url, method: 'GET', params});
+        res.send(response.data);
+        
+    } catch (error) {
+        res.send({message: error})
+    }    
 }
    
 //define routes
