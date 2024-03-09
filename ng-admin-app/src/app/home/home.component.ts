99955b266cf4810504e4e47ae66e642bd34fa06f
Dan Newingham <dannewingham@gmail.com>
2024-03-09 11:33:59 +0000

    feat(ng-admin-app): Trying JsonPipe

--- ng-admin-app/src/app/home/home.component.ts
+++ ng-admin-app/src/app/home/home.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [JsonPipe],
   template: `
     <main class="container">
       <p>
         home works!
       </p>
+      <div>
+        <p>Without JSON pipe:</p>
+        <pre>{{ testObject }}</pre>
+        <p>With JSON pipe:</p>
+        <pre>{{ testObject | json }}</pre>
+      </div>
     </main>
   `,
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  testObject: Object = {foo: 'bar', baz: 'qux', nested: {xyz: 3, numbers: [1, 2, 3, 4, 5]}};
 }
