edeacfee88c47ab66d63ff010eb2fa06a231580d
Dan Newingham <dannewingham@gmail.com>
2024-03-10 11:09:57 +0000

    test(ng-admin-app): Update AppComponent spec

--- ng-admin-app/src/app/app.component.spec.ts
+++ ng-admin-app/src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ng-admin-app');
+    expect(compiled.querySelector('a')?.textContent).toContain('Admin App');
   });
 });
