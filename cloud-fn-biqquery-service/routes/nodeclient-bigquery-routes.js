aaa5037357403784254e6cd0e8436542498003c8
Dan Newingham <dannewingham@gmail.com>
2024-03-13 21:00:07 +0000

    refac(cloud-fn-bigquery-svc): WIP troubleshooting access to Drive
    Cloud functions runtime doesn't seem to allow
    requesting specific auth scopes for service account.
    Since an auth scope for Drive access is required
    for the views referencing mock data, this refactor
    uses the google-auth-library which takes auth scopes.

--- cloud-fn-biqquery-service/routes/nodeclient-bigquery-routes.js
+++ cloud-fn-biqquery-service/routes/nodeclient-bigquery-routes.js
@@ -1,40 +1,15 @@
 const express = require("express");
 
-// Import google-auth-library for authentication and client for hitting GCP APIs.
-const {GoogleAuth} = require('google-auth-library');
+// Import the Google Cloud client library using default credentials
+const {BigQuery} = require('@google-cloud/bigquery');
 
 // Import helpers
 
 // Initialize constants
-///bigquery/v2/projects/{projectId}/jobs
-const bigqueryApiBaseUrl = 'https://bigquery.googleapis.com/bigquery/v2/projects/gcp-fhir-sandbox-lab-001/'
-const oauthOptions = {
-  scopes: ['https://www.googleapis.com/auth/cloud-platform', 'https://www.googleapis.com/auth/drive'],  //, 
-};
 
 // Define handlers
+const bigquery = new BigQuery();
 async function queryBigQuery(req, res) {
-  //initialize gcloud authorization client
-  const auth = new GoogleAuth(oauthOptions);
-  const client = await auth.getClient();
-
-  //const query = 'SELECT * FROM fhirstore_resource_streaming_d3_01.Patient LIMIT 2';
-  const query = 'SELECT * FROM mock_data.vw_fhir_conditions_publish'
-
-  //url: bigqueryApiBaseUrl + 'jobs/d33319e0-adfd-470c-a1f2-10e8b324df1e',
-  const requestOptions = {
-    url: bigqueryApiBaseUrl + 'queries',
-    method: 'POST',
-    data: {
-      query: query,
-      useLegacySql: false
-    }
-  };
-  //console.log(requestOptions);
-  const response = await client.request(requestOptions);
-  //console.log(JSON.stringify(response, null, 2));
-
-  /*
   //const query = `SELECT * FROM \`gcp-fhir-sandbox-lab-001.fhirstore_resource_streaming_d3_01.Patient\` LIMIT 2`;
   const query = `SELECT * FROM \`gcp-fhir-sandbox-lab-001.mock_data.vw_fhir_conditions_publish\` LIMIT 2`;
 
@@ -56,10 +31,7 @@ async function queryBigQuery(req, res) {
   console.log('Rows:');
   rows.forEach(row => console.log(JSON.stringify(row, null, 2)));
 
-  */
-  //res.json(rows);
-  res.json(response);
-  
+  res.json(rows);
 }
 
 // Initialize Router object from Express and define routes
@@ -72,11 +44,3 @@ router.get("/", queryBigQuery);
 
 // Export the configured router object
 module.exports = router;
-
-/*
-try {
-  queryBigQuery();
-} catch (err) {
-  console.log(JSON.stringify(err, null, 2));
-}
-*/
