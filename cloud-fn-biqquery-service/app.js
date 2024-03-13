1197ef1c0eea7124eb14c7c18d4dadaf95d5c555
Dan Newingham <dannewingham@gmail.com>
2024-03-13 10:11:09 +0000

    feat(cf-bigquery-service): Add express app around queryBigQuery handler

--- cloud-fn-biqquery-service/app.js
+++ cloud-fn-biqquery-service/app.js
@@ -1,29 +1,18 @@
-// Import the Google Cloud client library using default credentials
-const {BigQuery} = require('@google-cloud/bigquery');
-const bigquery = new BigQuery();
-async function query() {
-  // Queries the U.S. given names dataset for the state of Texas.
+//import 3rd party packages and libraries
+const express = require('express');
 
-  //const query = `SELECT * FROM \`gcp-fhir-sandbox-lab-001.fhirstore_resource_streaming_d3_01.Patient\` LIMIT 1`;
-  const query = `SELECT * FROM \`gcp-fhir-sandbox-lab-001.mock_data.vw_fhir_conditions_publish\` LIMIT 1`;
+//import our own files
+const bigqueryRoutes = require("./routes/bigquery-routes");
 
-  // For all options, see https://cloud.google.com/bigquery/docs/reference/rest/v2/jobs/query
-  const options = {
-    query: query,
-    // Location must match that of the dataset(s) referenced in the query.
-    location: 'US',
+// Create an Express object and routes (in order)
+const app = express();
+app.use('/bigquery/', bigqueryRoutes);
+app.use('/', (req, res) => { res.status(200).send('Hello World'); })  //root requires 200 response to trigger automated testing with start-server-and-test package
+
+//handle route not found
+app.use((req, res) => { res.status(404).send('Bad URL'); });
+
+// Set our GCF handler to our Express app.
+module.exports = {
+    app
   };
-
-  // Run the query as a job
-  const [job] = await bigquery.createQueryJob(options);
-  console.log(`Job ${job.id} started.`);
-
-  // Wait for the query to finish
-  const [rows] = await job.getQueryResults();
-
-  // Print the results
-  console.log('Rows:');
-  rows.forEach(row => console.log(JSON.stringify(row, null, 2)));
-}
-
-query();
