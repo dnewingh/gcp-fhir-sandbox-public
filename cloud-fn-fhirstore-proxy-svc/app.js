9853095c9bd016f34d0e314198854384c52620a8
Dan Newingham <dannewingham@gmail.com>
2024-03-18 14:50:09 +0000

    refac(cloud-fn-fhirstore-proxy-svc): Clean-up unused routes
    Last commit in troubleshooting series dealing with
    data loss for valid error status codes.  Key fixes:
    1. Setting responseType: 'text'
    2. Setting validateStatus: () => true

--- cloud-fn-fhirstore-proxy-svc/app.js
+++ cloud-fn-fhirstore-proxy-svc/app.js
@@ -3,19 +3,15 @@ const express = require('express');
 const cors = require('cors');
 
 //import our own files
-const userRoutes = require("./routes/user-routes");
 const fhirstoreRoutes = require("./routes/fhirstore-routes");
 const valueSetOperationsRoutes = require("./routes/valueset-operations-routes");
-const testRoutes = require("./routes/test-routes");
 
 // Create an Express object and routes (in order)
 const app = express();
 app.use(cors());
-app.use('/users/', userRoutes);
 app.use('/fhir/ValueSet', valueSetOperationsRoutes);
 app.use('/fhir/', fhirstoreRoutes);
-app.use('/test/', testRoutes);
-app.use('/', (req, res) => { res.status(200).send('Hello World'); })  //root requires 200 response to trigger automated testing with start-server-and-test package
+app.get('/', (req, res) => { res.status(200).send('Hello World'); })  //root requires 200 response to trigger automated testing with start-server-and-test package
 
 //handle route not found
 app.use((req, res) => { res.status(404).send('Bad URL'); });
