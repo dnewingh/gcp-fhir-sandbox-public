41b684ce39bae708edadd79892fc547fbd058666
Dan Newingham <dannewingham@gmail.com>
2024-03-21 19:38:05 +0000

    feat(ng-admin-app): Add request logs component

--- ng-admin-app/src/app/request-logs/request-logs.component.ts
+++ ng-admin-app/src/app/request-logs/request-logs.component.ts
@@ -1,26 +1,23 @@
 import { Component } from '@angular/core';
 import { RequestLogsEntry, RequestLogsService } from '../services/request-logs.service';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { JsonPipe } from '@angular/common';
+import { DatePipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-request-logs',
   standalone: true,
-  imports: [ NgbAccordionModule, JsonPipe ],
+  imports: [ NgbAccordionModule, JsonPipe, DatePipe ],
   template: `
-    <p>
-      request-logs works! ({{ requestLogs.length }})
-    </p>
-    <div ngbAccordion>
+    <h2>Request Logs</h2>
+    
+    <div ngbAccordion class="mb-3">
       @for (logEntry of requestLogs; track logEntry.mockResourceId) {
         <div ngbAccordionItem>
-          <div ngbAccordionHeader>
-            <button ngbAccordionButton class="justify-content-between">
-              <div>
-                {{ logEntry.requestMethod}} {{ logEntry.relativeUrl }}
-              </div>
-              <div>
-                Response Status: {{ logEntry.responseStatus }}
+          <div ngbAccordionHeader class="p-1 accordion-button">
+            <button ngbAccordionToggle class="btn container-fluid text-start p-0 mx-1">
+              <div class="d-flex justify-content-between small">
+                <div>{{ logEntry.requestTimestamp | date: 'mediumTime' }} {{ logEntry.requestMethod}} {{ logEntry.relativeUrl }}</div>
+                <div>Response Status: {{ logEntry.responseStatus }}</div>
               </div>
             </button>
           </div>
@@ -30,19 +27,25 @@ import { JsonPipe } from '@angular/common';
             </div>
           </div>
         </div>
+      } @empty {
+        <p class="mb-0">There are no requests yet.</p>
       }
       
     </div>
       
     
   `,
-  styles: ``
+  styles: `
+    .custom-header::after {
+      content: none;
+    }
+  `
 })
 export class RequestLogsComponent {
   requestLogs: RequestLogsEntry[] = [];
 
   constructor(private requestLogsService: RequestLogsService) {}
-
+  
   ngOnInit() {
     this.requestLogs = this.requestLogsService.getLogs().sort((a, b) => {
       return b.requestTimestamp.getTime() - a.requestTimestamp.getTime(); // Sort in descending order by timestamp
