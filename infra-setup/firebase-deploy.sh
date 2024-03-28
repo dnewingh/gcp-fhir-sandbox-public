e54c1065d7f00f70fe15f5fb66e4c1a29aa338b4
Dan Newingham <dannewingham@gmail.com>
2024-03-28 21:05:20 +0000

    feat(infra-setup): Create environment file, build, and deploy to firebase

--- infra-setup/firebase-deploy.sh
+++ infra-setup/firebase-deploy.sh
@@ -19,16 +19,14 @@ USER_EMAIL="$1"
 PROJECT_ID="$2" #gcp-fhir-sandbox-001
 
 # Declare constants
-DATASET_NAME=GCP-FHIR-Sandbox-001-Healthcare-Dataset-001
-FHIRSTORE_NAME=FHIRSTORE-001
 LOCATION=us-central1
-##BQ_STREAMING_DATASET_NAME=fhirstore_resource_streaming_01
 
 # Create production environment file
+echo "Preparing production environment.ts file..."
 
 # Assign arguments to variables
 bigQueryServiceUrl="https://$LOCATION-$PROJECT_ID.cloudfunctions.net/nodejs-http-fn-biqquery-service/bigquery/"
-fhirstoreServiceUrl="https://us-central1-gcp-fhir-sandbox-lab-001.cloudfunctions.net/nodejs-http-fn-fhirstore-proxy-service/fhir/"
+fhirstoreServiceUrl="https://$LOCATION-$PROJECT_ID.cloudfunctions.net/nodejs-http-fn-fhirstore-proxy-service/fhir/"
 
 # Define the content of the environment.ts file
 environment_content=$(cat <<EOF
@@ -41,21 +39,18 @@ EOF
 )
 
 # Specify the file path
-file_path="environment.ts"
-
-# Check if the file already exists
-if [ -f "$file_path" ]; then
-    echo "Error: File '$file_path' already exists."
-    exit 1
-fi
+file_path="src/environments/environment.ts"
 
 # Create the environment.ts file and write the content
 echo "$environment_content" > "$file_path"
 
 # Display a message indicating successful creation
 echo "environment.ts file created successfully."
-
-exit 1 
+ 
 # Build and deploy application to firebase
+echo "Building app..."
+npm install
 npm run build
+
+echo "Deploying to firebase..."
 firebase deploy --only hosting
