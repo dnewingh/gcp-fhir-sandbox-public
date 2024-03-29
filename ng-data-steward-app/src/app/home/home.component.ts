5b6257351240ca30db7ad68a5566de234bb1f0be
Dan Newingham <dannewingham@gmail.com>
2024-03-29 17:27:04 +0000

    feat(ng-data-steward-app): Display sheetsUrl

--- ng-data-steward-app/src/app/home/home.component.ts
+++ ng-data-steward-app/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HomeMockDataTableComponent } from '../home-mock-data-table/home-mock-data-table.component';
 import { RequestLogsComponent } from '../request-logs/request-logs.component';
+import { BigqueryMockDataService } from '../services/bigquery-mock-data.service';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import { RequestLogsComponent } from '../request-logs/request-logs.component';
   template: `
     <main class="container">
       <app-request-logs></app-request-logs>
+      <p class="mb-1 fw-light fst-italic fs-6">{{ sheetsUrl.sheetsUrl }}</p>
       <app-home-mock-data-table resourceType="Patient" mockDataTableName="vw_fhir_patients_publish"></app-home-mock-data-table>
       <app-home-mock-data-table resourceType="Condition" mockDataTableName="vw_fhir_conditions_publish"></app-home-mock-data-table>
     </main>
@@ -16,5 +18,18 @@ import { RequestLogsComponent } from '../request-logs/request-logs.component';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  
+  sheetsUrl?: any
+
+  constructor(private BigqueryMockDataService: BigqueryMockDataService) {}
+
+  ngOnInit() {
+    this.BigqueryMockDataService.getMockData('sheetsUrl').subscribe(
+      {
+        next: data => this.sheetsUrl = data,
+        error: error => console.error(error),
+        complete: () => console.log('SheetsUrl=' + this.sheetsUrl)
+      }
+    );
+  }
+
 }
