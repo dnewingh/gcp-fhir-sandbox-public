f460982b1935b98fbf7eb786d7b38935043b4185
Dan Newingham <dannewingham@gmail.com>
2024-03-29 09:40:13 +0000

    fix(bigquery-ddls): Fix bug introduced in vw_fhir_conditions_transform_001

--- big-query-ddls/views/vw_fhir_conditions_transform_001.sql
+++ big-query-ddls/views/vw_fhir_conditions_transform_001.sql
@@ -4,7 +4,7 @@ publishable
 , mockDataRecordId as id
 , mock_data.CreatesSingleEntryCodeableConceptObject(clinicalStatusCodingSystem, null, clinicalStatusCodingCode, clinicalStatusCodingDisplay, null, clinicalStatusCodingDisplay) as clinicalStatus
 , mock_data.CreatesSingleEntryCodeableConceptObject(verificationStatusCodingSystem, null, verificationStatusCodingCode, verificationStatusCodingDisplay, null, verificationStatusCodingDisplay) as verificationStatus
-, mock_data.CreatesSingleEntryCodeableConceptObject(categoryCodingSystem, null, categoryCodingCode, categoryCodingDisplay, null, categoryCodingDisplay) as category
+, [mock_data.CreatesSingleEntryCodeableConceptObject(categoryCodingSystem, null, categoryCodingCode, categoryCodingDisplay, null, categoryCodingDisplay)] as category
 , mock_data.CreatesSingleEntryCodeableConceptObject(codeCodingSystem, null, codeCodingCode, codeCodingDisplay, null, codeCodingDisplay) as code
 , mock_data.CreatesSimpleReferenceObject(subjectReference, null) as subject
  FROM `mock_data.raw_conditions`
